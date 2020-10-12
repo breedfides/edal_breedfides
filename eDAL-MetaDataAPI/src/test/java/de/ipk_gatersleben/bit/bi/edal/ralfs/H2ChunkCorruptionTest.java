28e5baeba3adec998b964da195277178b7c2f806
ralfs <ralfs@bit-58.ipk-gatersleben.de>
2020-10-12 12:33:02 +0200

    Seperate Testcase for H2 Bug testing

--- eDAL-MetaDataAPI/src/test/java/de/ipk_gatersleben/bit/bi/edal/ralfs/H2ChunkCorruptionTest.java
+++ eDAL-MetaDataAPI/src/test/java/de/ipk_gatersleben/bit/bi/edal/ralfs/H2ChunkCorruptionTest.java
@@ -1,4 +1,3 @@
-package de.ipk_gatersleben.bit.bi.edal.ralfs;
 /**
  * Copyright (c) 2020 Leibniz Institute of Plant Genetics and Crop Plant Research (IPK), Gatersleben, Germany.
  *
@@ -11,20 +10,36 @@ package de.ipk_gatersleben.bit.bi.edal.ralfs;
  *  Contributors:
  *       Leibniz Institute of Plant Genetics and Crop Plant Research (IPK), Gatersleben, Germany
  */
+package de.ipk_gatersleben.bit.bi.edal.ralfs;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.ServerSocket;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import de.ipk_gatersleben.bit.bi.edal.primary_data.DataManager;
+import de.ipk_gatersleben.bit.bi.edal.primary_data.EdalConfiguration;
+import de.ipk_gatersleben.bit.bi.edal.primary_data.EdalConfigurationException;
+import de.ipk_gatersleben.bit.bi.edal.primary_data.file.EdalException;
 import de.ipk_gatersleben.bit.bi.edal.primary_data.file.PrimaryDataDirectory;
-import de.ipk_gatersleben.bit.bi.edal.primary_data.file.PrimaryDataDirectoryException;
 import de.ipk_gatersleben.bit.bi.edal.primary_data.file.PrimaryDataFile;
 import de.ipk_gatersleben.bit.bi.edal.primary_data.metadata.EdalLanguage;
 import de.ipk_gatersleben.bit.bi.edal.primary_data.metadata.EnumDublinCoreElements;
@@ -35,16 +50,37 @@ import de.ipk_gatersleben.bit.bi.edal.primary_data.metadata.NaturalPerson;
 import de.ipk_gatersleben.bit.bi.edal.primary_data.metadata.Persons;
 import de.ipk_gatersleben.bit.bi.edal.primary_data.metadata.Subjects;
 import de.ipk_gatersleben.bit.bi.edal.primary_data.metadata.UntypedData;
+import de.ipk_gatersleben.bit.bi.edal.sample.EdalHelpers;
+import de.ipk_gatersleben.bit.bi.edal.test.EdalDefaultTestCase;
+import de.ipk_gatersleben.bit.bi.edal.test.EdalTestCaseWithoutShutdown;
 
-public class Inserterr {
+class H2ChunkCorruptionTest extends EdalTestCaseWithoutShutdown {
 	
+//	private static int HTTPS_PORT = 8443;
+//	private static int HTTP_PORT = 8080;
+//	private static final String ROOT_USER = "eDAL0815@ipk-gatersleben.de";
+//	private static final String EMAIL = "user@nodomain.com.invalid";
+//	private static final String DATACITE_PREFIX = "10.5072";
+//	private static final String DATACITE_PASSWORD = "";
+//	private static final String DATACITE_USERNAME = "";
+//	private static final int MIN_PORT_NUMBER = 49152;
+//	private static final int MAX_PORT_NUMBER = 65535;
+//	public EdalConfiguration configuration = null;
+//	public Path mountPath = null;
 	private PrimaryDataDirectory rootDirectory;
-	
-	public Inserterr(PrimaryDataDirectory root) {
-		this.rootDirectory = root;
-	}
 
-	public void process(PrimaryDataDirectory rootDirectory, int size) throws Exception {
+	@Test
+	void generateDataTest() throws Exception {
+		rootDirectory = DataManager.getRootDirectory(
+				EdalHelpers.getFileSystemImplementationProvider(true,
+						this.configuration), EdalHelpers
+						.authenticateWinOrUnixOrMacUser());
+    	createAndInsert(100000);
+    	DataManager.shutdown();
+	}
+	
+	
+	private void createAndInsert(int size) throws Exception {
 		ArrayList<String> names = getList("src/test/resources/names.txt");
 		ArrayList<String> words = getList("src/test/resources/words.txt");
 		Path pathToRessource = Paths.get("src/test/resources/_TEST.zip");
@@ -59,16 +95,14 @@ public class Inserterr {
 		PrimaryDataDirectory currentDirectory = null;
 		String archiveName = "";
 		for(int i = 0; i < size; i++) {
-			if(i%10000 == 0) {
-				archiveName = "Libraryp."+(i/10000);
+			if(i%1000 == 0) {
+				archiveName = "LibraryV7."+(i/1000);
 				currentDirectory = rootDirectory.createPrimaryDataDirectory(archiveName);
 			}
 			PrimaryDataFile entity = currentDirectory.createPrimaryDataFile("Entityp.."+i);
 			MetaData metadata = entity.getMetaData();
 			Persons persons = new Persons();
-			NaturalPerson np = new NaturalPerson(names.get(random.nextInt(countNames)),
-					names.get(random.nextInt(countNames)),words.get(random.nextInt(countWords)),
-					words.get(random.nextInt(countWords)),words.get(random.nextInt(countWords)));
+			NaturalPerson np = new NaturalPerson(names.get(random.nextInt(countNames)),names.get(random.nextInt(countNames)),words.get(random.nextInt(countWords)),words.get(random.nextInt(countWords)),words.get(random.nextInt(countWords)));
 			persons.add(np);
 			metadata.setElementValue(EnumDublinCoreElements.CREATOR, persons);
 //			metadata.setElementValue(EnumDublinCoreElements.PUBLISHER,new LegalPerson(
@@ -83,31 +117,75 @@ public class Inserterr {
 			metadata.setElementValue(EnumDublinCoreElements.TITLE, new UntypedData(words.get(random.nextInt(countWords))));
 			//metadata.setElementValue(EnumDublinCoreElements.DESCRIPTION, new UntypedData(words.get(random.nextInt(countWords))));
 			//metadata.setElementValue(EnumDublinCoreElements.IDENTIFIER, referenceIdentifier);
-			entity.setMetaData(metadata);
-			//entity.store(fin);
-			log(archiveName+i+"/"+size+" Saved");
+			//entity.setMetaData(metadata);
+			entity.store(fin);
+			log(archiveName+"_ "+i+"/"+size+" Saved");
 		}
 		fin.close();
 	}
 	
-	
-	public String getSubfileTitle(PrimaryDataDirectory root) throws Exception {
-		ArrayList<String> names = getList("src/test/resources/names.txt");
-		PrimaryDataDirectory subdir;
-		try {
-			subdir = root.createPrimaryDataDirectory("subdirectory");
-		} catch (PrimaryDataDirectoryException e) {
-			subdir = (PrimaryDataDirectory) root.searchByDublinCoreElement(EnumDublinCoreElements.TITLE, new UntypedData("subdirectory"), false, true).get(0);
-		}
-		Random random = new Random();
-		String name = "subfile"+names.get(random.nextInt(names.size()));
-		PrimaryDataFile subfile = subdir.createPrimaryDataFile("subfile"+name);
-		MetaData submetadata = subfile.getMetaData();
-		submetadata.setElementValue(EnumDublinCoreElements.TITLE, new UntypedData(name));
-		subfile.setMetaData(submetadata);
-		return name;
-	}
-	
+//	@BeforeEach
+//	public void setUp() throws Exception {
+//
+//		int port = ThreadLocalRandom.current().nextInt(MIN_PORT_NUMBER, MAX_PORT_NUMBER);
+//
+//		while (!available(port)) {
+//			port = ThreadLocalRandom.current().nextInt(MIN_PORT_NUMBER, MAX_PORT_NUMBER);
+//		}
+//
+//		H2ChunkCorruptionTest.HTTP_PORT = port;
+//
+//		try {
+//			this.configuration = new EdalConfiguration(H2ChunkCorruptionTest.DATACITE_USERNAME,
+//					H2ChunkCorruptionTest.DATACITE_PASSWORD, H2ChunkCorruptionTest.DATACITE_PREFIX,
+//					new InternetAddress(H2ChunkCorruptionTest.EMAIL),
+//					new InternetAddress(H2ChunkCorruptionTest.EMAIL),
+//					new InternetAddress(H2ChunkCorruptionTest.EMAIL),
+//					new InternetAddress(H2ChunkCorruptionTest.ROOT_USER)
+//					,"imap.ipk-gatersleben.de","","");
+//			this.configuration.setHttpPort(H2ChunkCorruptionTest.HTTP_PORT);
+//			this.configuration.setHttpsPort(H2ChunkCorruptionTest.HTTPS_PORT);
+//
+//			mountPath = Paths.get(System.getProperty("user.home"), "edaltest", "SEARCHTESTS");
+//			Files.createDirectories(mountPath);
+//
+//			this.configuration.setMountPath(mountPath);
+//			this.configuration.setDataPath(mountPath);
+//
+//		} catch (EdalConfigurationException | AddressException e) {
+//			throw new EdalException(e);
+//		}
+//	}
+//	
+//	public static boolean available(int port) {
+//		if (port < MIN_PORT_NUMBER || port > MAX_PORT_NUMBER) {
+//			throw new IllegalArgumentException("Invalid start port: " + port);
+//		}
+//
+//		ServerSocket ss = null;
+//		DatagramSocket ds = null;
+//		try {
+//			ss = new ServerSocket(port);
+//			ss.setReuseAddress(true);
+//			ds = new DatagramSocket(port);
+//			ds.setReuseAddress(true);
+//			return true;
+//		} catch (IOException e) {
+//		} finally {
+//			if (ds != null) {
+//				ds.close();
+//			}
+//
+//			if (ss != null) {
+//				try {
+//					ss.close();
+//				} catch (IOException e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		}
+//		return false;
+//	}
 	
 	public ArrayList<String> getList(String pathName) throws FileNotFoundException{
 	File file = new File(pathName);
@@ -123,6 +201,4 @@ public class Inserterr {
     	DataManager.getImplProv().getLogger().info(msg);
     }
 
-
 }
-
