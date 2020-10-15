6325c97ee429741f02754e7ad00fb04a352a1d48
ralfs <ralfs@bit-58.ipk-gatersleben.de>
2020-10-15 15:15:40 +0200

    Bridge to store a Long in an Index

---
+++ eDAL-MetaDataAPI/src/main/java/ralfs/de/ipk_gatersleben/bit/bi/edal/examples/LongBridge.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2020 Leibniz Institute of Plant Genetics and Crop Plant Research (IPK), Gatersleben, Germany.
+ *
+ * We have chosen to apply the GNU General Public License (GPL) Version 3 (https://www.gnu.org/licenses/gpl-3.0.html)
+ * to the copyrightable parts of e!DAL, which are the source code, the executable software, the training and
+ * documentation material. This means, you must give appropriate credit, provide a link to the license, and indicate
+ * if changes were made. You are free to copy and redistribute e!DAL in any medium or format. You are also free to
+ * adapt, remix, transform, and build upon e!DAL for any purpose, even commercially.
+ *
+ *  Contributors:
+ *       Leibniz Institute of Plant Genetics and Crop Plant Research (IPK), Gatersleben, Germany
+ */
+package ralfs.de.ipk_gatersleben.bit.bi.edal.examples;
+
+
+import java.util.Locale;
+
+import org.hibernate.search.mapper.pojo.bridge.ValueBridge;
+import org.hibernate.search.mapper.pojo.bridge.runtime.ValueBridgeToIndexedValueContext;
+
+import de.ipk_gatersleben.bit.bi.edal.primary_data.metadata.implementation.MyEdalLanguage;
+
+public class LongBridge implements ValueBridge<Long, String> {
+
+	@Override
+	public String toIndexedValue(Long value, ValueBridgeToIndexedValueContext context) {
+		return Long.toString(value);
+	}
+} 
