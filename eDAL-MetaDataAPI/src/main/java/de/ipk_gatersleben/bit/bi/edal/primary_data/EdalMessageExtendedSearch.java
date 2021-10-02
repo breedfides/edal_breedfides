20f81466e898a92235abcce40f4ef7aacd74d3b4
Ralfs <ralfs@ipk-gatersleben.de>
2021-10-02 16:06:36 +0200

    Working on a better scaling faceted search with Lucenes facet package

--- eDAL-MetaDataAPI/src/main/java/de/ipk_gatersleben/bit/bi/edal/primary_data/EdalMessageExtendedSearch.java
+++ eDAL-MetaDataAPI/src/main/java/de/ipk_gatersleben/bit/bi/edal/primary_data/EdalMessageExtendedSearch.java
@@ -12,7 +12,12 @@ package de.ipk_gatersleben.bit.bi.edal.primary_data;
  *       Leibniz Institute of Plant Genetics and Crop Plant Research (IPK), Gatersleben, Germany
  */
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -37,6 +42,8 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.DateTools;
@@ -53,6 +60,8 @@ import org.apache.lucene.queryparser.xml.builders.BooleanQueryBuilder;
 import org.apache.lucene.search.BooleanClause.Occur;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.glassfish.jersey.server.ManagedAsync;
 import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.FuzzyQuery;
@@ -82,6 +91,7 @@ import de.ipk_gatersleben.bit.bi.edal.primary_data.file.implementation.PublicRef
 import de.ipk_gatersleben.bit.bi.edal.primary_data.file.implementation.PublicVersionIndexWriterThread;
 
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 
 @Path("extendedSearch")
 public class EdalMessageExtendedSearch {
@@ -143,5 +153,75 @@ public class EdalMessageExtendedSearch {
 		result.put(MetaDataImplementation.DESCRIPTION, VeloCityHtmlGenerator.getDescriptions());		
 		return result;
 	}
+	
+	@GET
+    @Path("/pdf")
+    public Response downloadPdfFile()
+    {
+        StreamingOutput fileStream =  new StreamingOutput() 
+        {
+            @Override
+            public void write(java.io.OutputStream output) throws IOException, WebApplicationException 
+            {
+                try
+                {
+                    java.nio.file.Path path = Paths.get("C:\\Program Files (x86)\\Google\\evx.zip");
+                    byte[] data = Files.readAllBytes(path);
+                    output.write(data);
+                    output.flush();
+                } 
+                catch (Exception e) 
+                {
+                    throw new WebApplicationException("File Not Found !!");
+                }
+            }
+        };
+        return Response
+                .ok(fileStream, MediaType.APPLICATION_OCTET_STREAM)
+                .header("content-disposition","attachment; filename = evx.zip")
+                .build();
+    }
+	
+	@POST
+	@Path("/upload")
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public Response uploadFile(
+		@FormDataParam("file") InputStream uploadedInputStream,
+		@FormDataParam("file") FormDataContentDisposition fileDetail) {
+
+		String uploadedFileLocation = "d://uploaded/" + fileDetail.getFileName();
+
+		// save it
+		writeToFile(uploadedInputStream, uploadedFileLocation);
+
+		String output = "File uploaded to : " + uploadedFileLocation;
+
+		return Response.status(200).entity(output).build();
+
+	}
+
+	// save uploaded file to new location
+	private void writeToFile(InputStream uploadedInputStream,
+		String uploadedFileLocation) {
+
+		try {
+			OutputStream out = new FileOutputStream(new File(
+					uploadedFileLocation));
+			int read = 0;
+			byte[] bytes = new byte[1024];
+
+			out = new FileOutputStream(new File(uploadedFileLocation));
+			while ((read = uploadedInputStream.read(bytes)) != -1) {
+				out.write(bytes, 0, read);
+			}
+			out.flush();
+			out.close();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+	
 
 }
