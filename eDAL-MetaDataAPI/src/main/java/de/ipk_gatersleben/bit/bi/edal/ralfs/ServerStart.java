b0fbb2afad70858459ad11c1a2162a1457c56ac4
Daniel Arend <arendd@ipk-gatersleben.de>
2021-12-16 10:36:04 +0100

    fix license headers

--- eDAL-MetaDataAPI/src/main/java/de/ipk_gatersleben/bit/bi/edal/ralfs/ServerStart.java
+++ eDAL-MetaDataAPI/src/main/java/de/ipk_gatersleben/bit/bi/edal/ralfs/ServerStart.java
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2021 Leibniz Institute of Plant Genetics and Crop Plant Research (IPK), Gatersleben, Germany.
+ *
+ * We have chosen to apply the GNU General Public License (GPL) Version 3 (https://www.gnu.org/licenses/gpl-3.0.html)
+ * to the copyrightable parts of e!DAL, which are the source code, the executable software, the training and
+ * documentation material. This means, you must give appropriate credit, provide a link to the license, and indicate
+ * if changes were made. You are free to copy and redistribute e!DAL in any medium or format. You are also free to
+ * adapt, remix, transform, and build upon e!DAL for any purpose, even commercially.
+ *
+ *  Contributors:
+ *       Leibniz Institute of Plant Genetics and Crop Plant Research (IPK), Gatersleben, Germany
+ */
 package de.ipk_gatersleben.bit.bi.edal.ralfs;
 
 import java.io.BufferedWriter;
